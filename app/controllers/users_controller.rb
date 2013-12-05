2dd7d2de8bcafa9aad6bfdf5177e1e84676c625e
John Richardson <richardsonjm@gmail.com>
2013-12-04 19:20:26 -0500

    added redirect to user page after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,15 +30,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      login @user.id
+      redirect_to @user
+    else
+      render 'new'
     end
+
+
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render action: 'show', status: :created, location: @user }
+    #   else
+    #     format.html { render 'new' }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /users/1
